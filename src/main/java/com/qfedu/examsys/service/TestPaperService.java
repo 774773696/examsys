7943606fea1597c7e502ae380f29d4d25a81f493
ddpk <774773696@qq.com>
2019-10-16 11:41:22 +0800

    单选题遍历显示

--- src/main/java/com/qfedu/examsys/service/TestPaperService.java
+++ src/main/java/com/qfedu/examsys/service/TestPaperService.java
@@ -1,5 +1,6 @@
 package com.qfedu.examsys.service;
 
+import com.qfedu.examsys.pojo.ESinglequestions;
 import com.qfedu.examsys.pojo.ESubject;
 
 import java.util.List;
@@ -7,4 +8,10 @@ import java.util.List;
 public interface TestPaperService {
     //查询所有试题的科目
     public List<ESubject> findAllSubject();
+
+    //查询所有的单选题目
+    public List<ESinglequestions> findAllSingleQuestions();
+
+    public void importSinglequestions();
+
 }
