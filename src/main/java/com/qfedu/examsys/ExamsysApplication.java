c0da1fd666a6c28983b5ffd97cad511072c4ad49
ddpk <774773696@qq.com>
2019-10-21 08:21:24 +0800

    打包报错解决

--- src/main/java/com/qfedu/examsys/ExamsysApplication.java
+++ src/main/java/com/qfedu/examsys/ExamsysApplication.java
@@ -3,12 +3,20 @@ package com.qfedu.examsys;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching //启用缓存
 @SpringBootApplication
 @MapperScan("com.qfedu.examsys.dao")
-public class ExamsysApplication {
+public class ExamsysApplication extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(ExamsysApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ExamsysApplication.class, args);
